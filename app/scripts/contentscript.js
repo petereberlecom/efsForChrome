970a215bb575de222185b1de5717a9a40d87bae7
petereberlecom <me@peter-eberle.com>
2016-04-29 16:10:07 +0200

    fuck off windows

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,11 @@
 'use strict';
+
+
+$('body').append('<div class="respondiToolbar"></div>')
+$('.respondiToolbar').append('<div class="respondiEFS"><div class="image"><img src="http://www.respondi.com/wp-content/uploads/2014/04/respondi-logo.png" alt=""><h3 class="claim">efs for respondi</h3></div></div>');
+
+
+
+$('.tablebutton ').parent('td').addClass('buttonWrapper');
+
+
